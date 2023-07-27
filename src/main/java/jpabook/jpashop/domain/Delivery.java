2f6c5c7130b9944cc16751d79f9cbcd2905c0444
yonguk <wldyddnr@gmail.com>
2023-07-27 21:13:15 +0900

    @Embeddable, @Embedded

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,9 +1,6 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
 
@@ -13,9 +10,8 @@ public class Delivery extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     private DeliveryStatus status;
 
@@ -30,29 +26,6 @@ public class Delivery extends BaseEntity{
         this.id = id;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
 
     public DeliveryStatus getStatus() {
         return status;
@@ -68,5 +41,13 @@ public class Delivery extends BaseEntity{
 
     public void setOrder(Order order) {
         this.order = order;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
